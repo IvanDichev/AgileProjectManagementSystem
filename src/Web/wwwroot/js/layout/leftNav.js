809c1ae3e5ebb3063f8a27ec7dcbc4821de869ca
IvanDichev <i.dichev0@gmail.com>
2020-11-10 16:39:58 +0200

    Refactor layout/nav css and scripts

---
+++ src/Web/wwwroot/js/layout/leftNav.js
@@ -0,0 +1,30 @@
+﻿// Push main content with leftNav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main-container").style.marginLeft = "0";
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    if (screen.width > 550) {
+        document.getElementById("main-container").style.marginLeft = "250px";
+    }
+}
+
+// Toggle left/rigth arrow icon for open/close leftNav
+$(document).ready(function () {
+    $(".close-nav").click(function (event) {
+        event.preventDefault();
+        $(".open-nav").show();
+        $(".close-nav").hide();
+    });
+    $(".open-nav").click(function (event) {
+        event.preventDefault();
+        $(".open-nav").hide();
+        $(".close-nav").show();
+    });
+});
