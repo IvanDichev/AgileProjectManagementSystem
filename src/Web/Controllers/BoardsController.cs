3491408efd14d4755fb6e2c35e0214706bb8662c
IvanDichev <i.dichev0@gmail.com>
2020-11-28 21:06:58 +0200

    Add js for draggable kanban elements

---
+++ src/Web/Controllers/BoardsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class BoardsController : BaseController
+    {
+        public IActionResult Board()
+        {
+            return View();
+        }
+    }
+}
