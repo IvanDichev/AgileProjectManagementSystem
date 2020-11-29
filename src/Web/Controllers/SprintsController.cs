cf2ef25c22a6239e584cce2f5a9bbe056703d38f
IvanDichev <i.dichev0@gmail.com>
2020-11-29 17:16:47 +0200

    Add sprints controller

---
+++ src/Web/Controllers/SprintsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class SprintsController : BaseController
+    {
+        public SprintsController()
+        {
+
+        }
+
+        public IActionResult Sprints()
+        {
+            return View();
+        }
+    }
+}
