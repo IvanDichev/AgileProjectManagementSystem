78d65e4e03abaef819573f31621113da80c0c055
IvanDichev <i.dichev0@gmail.com>
2020-12-07 14:53:49 +0200

    Add Board service interface

---
+++ src/Services/BoardColumns/IBoardService.cs
@@ -0,0 +1,15 @@
+﻿using DataModels.Models.Board;
+using DataModels.Models.Board.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Services.BoardColumns
+{
+    public interface IBoardService
+    {
+        Task<ICollection<BoardColumnAllDto>> GetAllColumnsAsync(int projectId);
+        Task<ICollection<BoardColumnAllDto>> GetAllColumnsForSprintAndProjectAsync(int projectId, int sprintId);
+        Task<ICollection<BoardColumnAllNamePositionDto>> GetColumnsNamesPositionAsync(int projectId);
+        Task AddcolumnToTheLeftAsync(BoardOptionsInputModel inputModel);
+    }
+}
