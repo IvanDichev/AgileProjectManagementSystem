58f100229a574c0c7b3347c6b3be702a0a827fc1
IvanDichev <i.dichev0@gmail.com>
2020-12-07 14:54:55 +0200

    Change references of kanban boards

---
+++ src/Data/Migrations/20201206230547_RemoveRefFromSprintsToKanbarnBoardColumns.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class RemoveRefFromSprintsToKanbarnBoardColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sprints_KanbanBoardColumns_KanbanBoardId",
+                table: "Sprints");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sprints_KanbanBoardId",
+                table: "Sprints");
+
+            migrationBuilder.DropColumn(
+                name: "KanbanBoardId",
+                table: "Sprints");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KanbanBoardId",
+                table: "Sprints",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sprints_KanbanBoardId",
+                table: "Sprints",
+                column: "KanbanBoardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sprints_KanbanBoardColumns_KanbanBoardId",
+                table: "Sprints",
+                column: "KanbanBoardId",
+                principalTable: "KanbanBoardColumns",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
