114925d1b9055757af70fdcc9b4285479c4dda2a
IvanDichev <i.dichev0@gmail.com>
2020-12-14 12:04:41 +0200

    Add Wiki area

---
+++ src/Web/Areas/Wiki/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Controllers;
+
+namespace Web.Areas.Wiki.Controllers
+{
+    [Area("Wiki")]
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
