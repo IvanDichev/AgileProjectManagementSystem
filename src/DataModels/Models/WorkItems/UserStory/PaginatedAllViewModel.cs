6bea33a0a6aca7f44beb892bbcef9e7ea4f0ca39
IvanDichev <i.dichev0@gmail.com>
2020-12-16 20:56:35 +0200

    Add paginated model for workitems

---
+++ src/DataModels/Models/WorkItems/UserStory/PaginatedAllViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DataModels.Models.WorkItems.UserStory
+{
+    public class PaginatedAllViewModel
+    {
+        public UserStoryAllViewmodel ViewModel { get; set; }
+
+        public int RecordsPerPage { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
